7de352821d0c2b7ff647d96f90a1d0938332720d
Gianluca Fiore <forod.g@gmail.com>
2014-01-01 09:01:57 +0100

    Update copyright year

--- renamer.py
+++ renamer.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 ###############################################################################
-# Copyright (c) 2010-2013, Gianluca Fiore <forod.g@gmail.com>
+# Copyright (c) 2010-2014, Gianluca Fiore <forod.g@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
