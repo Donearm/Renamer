44116e135e9165d46a9ae638e6bc19d6bb911b23
Gianluca Fiore <gianlucafiore@papersounds.eu>
2020-01-05 14:46:19 +0100

    update copyright years

--- renamer.py
+++ renamer.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 ###############################################################################
-# Copyright (c) 2010-2015, Gianluca Fiore <forod.g@gmail.com>
+# Copyright (c) 2010-2020, Gianluca Fiore <forod.g@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
